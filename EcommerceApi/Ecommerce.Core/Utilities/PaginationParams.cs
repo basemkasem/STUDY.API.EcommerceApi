963bb4cd021a3dacc2f3ee983fb35254615bb2c7
basem <basem.kasem16@gmail.com>
2026-03-08 23:38:44 +0200

    fix page number issue when entered less than 1

--- EcommerceApi/Ecommerce.Core/Utilities/PaginationParams.cs
+++ EcommerceApi/Ecommerce.Core/Utilities/PaginationParams.cs
@@ -3,7 +3,13 @@ namespace Ecommerce.Core.Utilities;
 public class PaginationParams
 {
     private const int DefaultPageSize = 50;
-    public int PageNumber { get; set; } = 1;
+
+    public int PageNumber
+    {
+        get;
+        init => 
+            field = value > 0 ? value : 1;
+    } = 1;
 
     public int PageSize
     {
